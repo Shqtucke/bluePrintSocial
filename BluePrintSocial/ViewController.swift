35e204f2e06581313f1e0f806e61c6851f8d2058
Shaun Tucker <tucke@cpe-2606-a000-1319-a0c4-0-b056-6bfd-a02f.dyn6.twc.com>
2018-06-17 12:57:44 -0400

    Initial Commit

---
+++ BluePrintSocial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BluePrintSocial
+//
+//  Created by Shaun Tucker on 6/17/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

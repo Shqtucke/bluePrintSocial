dec46a258206cb308785c7eced531f942378941e
Shaun Tucker <tucke@shauns-air.home>
2018-06-19 19:05:45 -0400

    minor roundbutton change

--- BluePrintSocial/View/RoundButton.swift
+++ BluePrintSocial/View/RoundButton.swift
@@ -13,7 +13,7 @@ class RoundButton: UIButton {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)

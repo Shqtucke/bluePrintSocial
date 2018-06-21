1f45a4d8471090c4cc706e1ed836a40c92fba6bb
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-06-21 19:12:10 -0400

    FeedVC constraints good enough

---
+++ BluePrintSocial/View/CircleButton.swift
@@ -0,0 +1,28 @@
+//
+//  CircleButton.swift
+//  BluePrintSocial
+//
+//  Created by Shaun Tucker on 6/21/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.shadowColor = UIColor(red: 0/255, green: 0/255, blue: 0/255, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        layer.cornerRadius = self.frame.width / 2
+        
+        
+    }
+
+}
--- BluePrintSocial/View/FancyView.swift
+++ BluePrintSocial/View/FancyView.swift
@@ -13,10 +13,11 @@ class FancyView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
         
-//        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
-//        layer.shadowOpacity = 0.8
-//        layer.shadowRadius = 5.0
-//        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.shadowColor = UIColor(red: SHADOW_GREY, green: SHADOW_GREY, blue: SHADOW_GREY, alpha: 0.6).cgColor
+        layer.shadowOpacity = 0.8
+        layer.shadowRadius = 5.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+        layer.cornerRadius = 2 
     }
 
 }

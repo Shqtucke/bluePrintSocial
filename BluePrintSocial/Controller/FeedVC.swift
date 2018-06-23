f8b08740832349ff4ed47f0a07b5357fcedcb24d
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-06-23 08:33:55 -0400

    firebase get complete

--- BluePrintSocial/Controller/FeedVC.swift
+++ BluePrintSocial/Controller/FeedVC.swift
@@ -15,7 +15,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet var tableView: UITableView!
     
-
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,7 +27,18 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.reloadData()
         
         DataService.ds.REF_POSTS.observe(.value) { (snapshot) in
-            print(snapshot.value)
+            //print(snapshot.value)
+            if let snapshot = snapshot.children.allObjects as? [DataSnapshot] {
+                for snap in snapshot {
+                    print("SNAP: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData() 
         }
         
     }
@@ -46,9 +58,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print("TUCK: \(post.caption)")
+        
         return tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostCell
     }
 

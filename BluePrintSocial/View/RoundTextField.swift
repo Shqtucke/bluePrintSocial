71a16829c93d5ee853637909b161577bf8666c7c
Shaun Tucker <tucke@Shauns-MacBook-Air.local>
2018-06-26 21:59:01 -0400

    New signIn UI

---
+++ BluePrintSocial/View/RoundTextField.swift
@@ -0,0 +1,48 @@
+//
+//  RoundTextField.swift
+//  BluePrintSocial
+//
+//  Created by Shaun Tucker on 6/26/18.
+//  Copyright © 2018 Shaun Tucker. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundTextField: UITextField {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            layer.cornerRadius = cornerRadius
+            layer.masksToBounds = cornerRadius > 0
+            
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            layer.borderWidth = borderWidth
+            
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        didSet {
+            layer.borderColor = borderColor?.cgColor
+        }
+    }
+    
+    @IBInspectable var bgColor: UIColor? {
+        didSet {
+            backgroundColor = bgColor
+        }
+    }
+    
+    @IBInspectable var placeholderColor: UIColor? {
+        didSet {
+            let rawString = attributedPlaceholder?.string != nil ? attributedPlaceholder!.string : ""
+            let str = NSAttributedString(string: rawString, attributes: [kCTForegroundColorAttributeName as NSAttributedString.Key: placeholderColor!])
+            attributedPlaceholder = str 
+        }
+    }
+}
